a1c65e958b2a2419db9e48bdfadfade9cd780f7e
JoC4S <jmcsaez@gmail.com>
2019-05-13 15:10:27 +0200

    Update primera clase

--- src/Proyecto1.py
+++ src/Proyecto1.py
@@ -12,6 +12,13 @@ import time
 import math
 
 sense = SenseHat()
+global Gx,Gy,Gz, sample
+datosGx = ""
+sample = 0
+Gx = 0
+Gy = 0
+Gz = 0
+stop = 0
 
 SETTINGS_FILE = "RTIMULib"
 
@@ -43,7 +50,8 @@ print("Recommended Poll Interval: %dmS\n" % poll_interval)
 #Callback the thread para mostrar orientación por pantalla
 def showrow():
     estado = 0;
-    while True:
+    while (stop == 0):
+        timeIni = int(round(time.time() * 1000))
         #Se define el angulo de la imagen a mostrar.
         if (Gz >= 0.5 and Gz > 0):
             img = sense.load_image("dentro.png")
@@ -62,53 +70,46 @@ def showrow():
         #Se muestra la imagen RGB
         if (estado != sense.rotation):
             sense.set_pixels(img)
-    return
+        timeEnd = int(round(time.time() * 1000))
+        c = (timeEnd - timeIni)
+        #print ("Gx: %f - Gy: %f - Gz: %f - Time: %i" % (Gx,Gy,Gz,c))
 
 def saveData():
-    print(" ", end = "\n")
-    print("Esperando joystick",end='\n')
+    global stop #Se utiliza para detener la ejecucion del programa cuando acba el thread
+    stop = 0
+    milli_secIni = int(round(time.time() * 1000))
+    print("\nEsperando joystick\n")
     event = sense.stick.wait_for_event()
     fGx = open("GxData.txt", "a")
     fGx.write(datosGx)
+    milli_secEnd = int(round(time.time() * 1000))
     fGx.close()
-    dtimend = datetime.now()
-    timetotal = dtimeini.microsecond - dtimend.microsecond
-    mps = (dtimeini.microsecond)
-    print("Datos grabados en GxData.txt. Tiempo = %i us. Muestras = %i" % (timetotal,sample),end='\n')
-    print("Tiempo = %i us. Tiempo end = %i" % (dtimeini.microsecond,dtimend.microsecond), end = "\n")
-    print("SPS = %f" % sample, end = "\n")
-    
+    milli_secTotal = int(milli_secEnd-milli_secIni)
+    SPS = sample/(milli_secTotal/1000)
+    print("Datos grabados en GxData.txt. Tiempo = %i ms. Muestras = %i, SPS = %f" % (milli_secTotal,sample,SPS),end='\n')
+    stop = 1
     return
 
-#configuracion del thread
-global datosGx, Gx, Gy, Gz, sample, timeini, timend
-datosGx = ""
-sample = 0
-Gx = 0
-Gy = 0
-Gz = 0
-
-threading.Thread(target=saveData).start()
-threading.Thread(target=showrow).start()
-
 #Bucle de ejecucion
-while True:
-  timeIni = datetime.now()
-  if imu.IMURead():
-    data = imu.getIMUData()
-    sample += 1
-    #Se obtiene el dato de aceleracion en la variable accel
-    accel = data["accel"]
-    Gx = accel[0]
-    Gy = accel[1]
-    Gz = accel[2]
-    #Se guardan los datos para tratamiento posterior
-    datosGx = datosGx + ("%f, " %Gx)
-    #Tiempo de espera
-    time.sleep(poll_interval*1.0/1000.0)
-    timeEnd = datetime.now()
-    c = (timeEnd - timeIni)/1000
-    print("Gx: %1.4f - Gy: %1.4f - Gz: %1.4f - Samp = %i - Time: %i ms " % (Gx,Gy,Gz,sample,c.microseconds),end='\r')
 
+#configuracion del threads
+threading.Thread(target=saveData).start()
+#threading.Thread(target=showrow).start()
 
-
+while (stop == 0):
+    timeIni = int(round(time.time() * 1000))
+    if imu.IMURead():
+        data = imu.getIMUData()
+        sample += 1
+        #Se obtiene el dato de aceleracion en la variable accel
+        accel = data["accel"]
+        Gx = accel[0]
+        Gy = accel[1]
+        Gz = accel[2]
+        #Se guardan los datos para tratamiento posterior
+        datosGx += ("%f, " %Gy)
+        #Tiempo de espera
+        #time.sleep(poll_interval*1.0/1000.0)
+        timeEnd = int(round(time.time() * 1000))
+        c = (timeEnd - timeIni)
+        print("Gx: %1.8f - Gy: %1.8f - Gz: %1.8f - Samp = %i - Time: %i ms.   " % (Gx,Gy,Gz,sample,c),end='\r')
