91d71444c5b411c3ab8b6c0ec9fc06ba9fa88156
JoC4S <jmcsaez@gmail.com>
2019-06-06 01:10:09 +0200

    Apartado 2 y 3 acabado

--- src/apartado2.py
+++ src/apartado2.py
@@ -18,14 +18,14 @@ import math
 sense = SenseHat()
 event = Event()
 
-Gx = 0          #almacena dato x Acelerometro
-Gy = 0          #almacena dato y Acelerometro
-Gz = 0          #almacena dato z Acelerometro
-timeIMU = 0     #Tiempo de proceso de la adquisicion de datos del IMU
-datosGx = ""    #Variable donde almacenar el array de muestras.
-datosGy = ""    #Variable donde almacenar el array de muestras.
-datosGz = ""    #Variable donde almacenar el array de muestras.
-stopthread3 = 0        #VFlag de parada de los procesos
+Gx = 0          # almacena dato x Acelerometro
+Gy = 0          # almacena dato y Acelerometro
+Gz = 0          # almacena dato z Acelerometro
+timeIMU = 0     # Tiempo de proceso de la adquisicion de datos del IMU
+datosGx = ""    # Variable donde almacenar el array de muestras.
+datosGy = ""    # Variable donde almacenar el array de muestras.
+datosGz = ""    # Variable donde almacenar el array de muestras.
+stopthread3 = 0        # VFlag de parada de los procesos
 stopthread2 = 0
 stopthread1 = 0
 
@@ -39,7 +39,7 @@ SETTINGS_FILE = "RTIMULib"
 
 print("Using settings file " + SETTINGS_FILE + ".ini")
 if not os.path.exists(SETTINGS_FILE + ".ini"):
-  print("Settings file does not exist, will be created")
+    print("Settings file does not exist, will be created")
 
 s = RTIMU.Settings(SETTINGS_FILE)
 imu = RTIMU.RTIMU(s)
@@ -59,21 +59,22 @@ imu.setGyroEnable(False)
 imu.setAccelEnable(True)
 imu.setCompassEnable(False)
 
-#Filtro passa bajos Apartado1
+# Filtro passa bajos Apartado1
 
 etapes = 20
 historyX = [0] * etapes
 historyY = [0] * etapes
 historyZ = [0] * etapes
 last_index = 0
-#coef = ar.array('i',[-435,-745,-1002,-884,-162,1224,3085,4997,6435,6970,6435,4997,3085,1224,-162,-884,-1002,-745,-435])
-coef = ar.array('i',[
+# coef = ar.array('i',[-435,-745,-1002,-884,-162,1224,3085,4997,6435,6970,6435,4997,3085,1224,-162,-884,-1002,-745,-435])
+coef = ar.array('i', [
      -526,   -720,   -727,   -449,    164,   1090,   2232,   3424,   4471,
      5189,   5444,   5189,   4471,   3424,   2232,   1090,    164,   -449,
      -727,   -720,   -526])
 
-def SampleFilter_put ():
-    global historyX,historyY, historyZ, last_index
+
+def SampleFilter_put():
+    global historyX, historyY, historyZ, last_index
     if (last_index == etapes):
         last_index = 0
     historyX[last_index] = Gx
@@ -81,15 +82,17 @@ def SampleFilter_put ():
     historyZ[last_index] = Gz
     last_index = last_index + 1
 
-#Funcion para la convolución
-def SampleFilter_get ():
-    global last_index, coef, accX,accY,accZ
+# Funcion para la convolución
+
+
+def SampleFilter_get():
+    global last_index, coef, accX, accY, accZ
 
     index = last_index
     i = 0
-    accX = 0
-    accY = 0
-    accZ = 0
+    accX = 0.00001
+    accY = 0.000001
+    accZ = 0.000001
     for i in range(etapes):
         if index != 0:
             index = index - 1
@@ -97,15 +100,18 @@ def SampleFilter_get ():
             index = etapes -1
         accX = accX + (historyX[index] * coef[i])
         accY = accY + (historyY[index] * coef[i])
-    accX = (accX / pow(2,16))*1.923
-    accY = (accY / pow(2,16))*1.923
-    accZ = (accZ / pow(2,16))*1.923
+        accZ = accZ + (historyZ[index] * coef[i])
+    accX = (accX / pow(2, 16))*1.925
+    accY = (accY / pow(2, 16))*1.925
+    accZ = (accZ / pow(2, 16))*1.925
     #print ("accX = %f , accY = %f , accZ = %f" %(accX, accY, accZ), end = '\r')
     return
 
-#Funcion para calculo del ángulo
+# Funcion para calculo del ángulo
+
+
 def angulos():
-    global angX, accX, accY, angY
+    global angX, accX, accY, accZ, angY
     if (accX > 1):
         accX = 1
     elif (accX < -1):
@@ -114,48 +120,75 @@ def angulos():
         accY = 1
     elif (accY < -1):
         accY = -1
-    angX = math.degrees(math.acos(accX)) -90
-    angY = math.degrees(math.acos(accY)) -90
+    angY = math.degrees(math.atan(accY/(math.sqrt(pow(accX,2)+pow(accZ,2)))))
+    if accZ > 0 and accY < 0:
+        angY = angY * -1
+    if accZ < 0 and accY < 0:
+        angY = 180 - abs(angY)
+    if accZ < 0 and accY > 0:
+        angY = 180 + angY
+    if accZ > 0 and accY > 0:
+        angY = 360 - angY
+
+    angX = math.degrees(math.atan(accX/(math.sqrt(pow(accY,2)+pow(accZ,2)))))
+    if accZ > 0 and accX < 0:
+        angX = angX * -1
+    if accZ < 0 and accX < 0:
+        angX = 180 - abs(angX)
+    if accZ < 0 and accX > 0:
+        angX = 180 + angX
+    if accZ > 0 and accX > 0:
+        angX = 360 - angX
+
+    #angX = math.degrees(math.acos(accX))
+    #angY = math.degrees(math.acos(accY))
+
 
 #########################################################################
 
-#Declaracion Thread 1
+# Declaracion Thread 1
+
+
 class myThread (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-   def run(self):
-      #Se ejecuta la función de adquisición de muestras
-      print ("Starting " + self.name)
-      while stopthread1 == 0:
-          adquisitionData(15)
-      print ("\nExiting " + self.name)
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
 
-#Declaracion Thread 2
+    def run(self):
+        # Se ejecuta la función de adquisición de muestras
+        print ("Starting " + self.name)
+        while stopthread1 == 0:
+            adquisitionData(15)
+        print ("\nExiting " + self.name)
+
+# Declaracion Thread 2
+
+
 class myThread2 (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-   def run(self):
-      print ("Starting " + self.name)
-      while stopthread2 == 0:
-          Dataget(12)
-      print ("\nExiting " + self.name)
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        print ("Starting " + self.name)
+        while stopthread2 == 0:
+            Dataget(12)
+        print ("\nExiting " + self.name)
 
 
-#Funcion del thread 1
+# Funcion del thread 1
 def adquisitionData(tiempo):
     t0 = time.time()
     SampleFilter_put()
-    SampleFilter_get ()
+    SampleFilter_get()
     angulos()
-    #Se setean el flag de thread para permitir al trhead de adquisición, tomar el dato.
+    # Se setean el flag de thread para permitir al trhead de adquisición, tomar el dato.
     event.set()
     time.sleep(tiempo/1000.0)
-    lapsetime = ((time.time() - t0) * 1000 )
-    print("Gx: %1.2f Gy: %1.2f Gz: %1.2f - angX = %2.1fº , angY = %2.1fº SampleTime = %f ms - IMUTime = %f" % (accX,accY,accZ,angX,angY,lapsetime, timeIMU), end = '\r')
+    lapsetime = ((time.time() - t0) * 1000)
+    print("Gx: %1.2f Gy: %1.2f Gz: %1.2f - angX = %2.1fº , angY = %2.1fº SampleTime = %f ms - IMUTime = %f" % (accX, accY, accZ, angX, angY, lapsetime, timeIMU), end = '\r')
 #Funcion del thread 2
 def Dataget (tiempo):
     global Gx, Gy, Gz, timeIMU
@@ -166,7 +199,7 @@ def Dataget (tiempo):
         time.sleep(0.25/1000.0)
 
     data = imu.getIMUData()
-    #Se obtiene el dato de aceleracion en la variable accel
+    # e obtiene el dato de aceleracion en la variable accel
     accel = data["accel"]
     Gx = accel[0]
     Gy = accel[1]
@@ -183,7 +216,6 @@ thread2 = myThread2(2, "Thread-2")
 # Start new Threads
 thread2.start()
 thread1.start()
-#thread4.start()
 
 
 print ("\nExiting Main Thread")
--- src/apartado3.py
+++ src/apartado3.py
@@ -3,9 +3,6 @@ from sense_hat import SenseHat
 from datetime import datetime
 from datetime import time
 from threading import Event, Thread
-import sys, getopt
-
-sys.path.append('.')
 import threading
 import _thread
 import RTIMU
@@ -13,30 +10,42 @@ import os.path
 import os
 import time
 import math
+import sys
+import getopt
+import array as ar
+
+
+sys.path.append('.')
 
 sense = SenseHat()
 event = Event()
 
-Gx = 0          #almacena dato x Acelerometro
-Gy = 0          #almacena dato y Acelerometro
-Gz = 0          #almacena dato z Acelerometro
-timeIMU = 0     #Tiempo de proceso de la adquisicion de datos del IMU
-numSamples = 0 #Numero de muestras tomadas
-datosGx = ""    #Variable donde almacenar el array de muestras.
-datosGy = ""    #Variable donde almacenar el array de muestras.
-datosGz = ""    #Variable donde almacenar el array de muestras.
-stopthread3 = 0        #VFlag de parada de los procesos
+Gx = 0          # almacena dato x Acelerometro
+Gy = 0          # almacena dato y Acelerometro
+Gz = 0          # almacena dato z Acelerometro
+timeIMU = 0     # Tiempo de proceso de la adquisicion de datos del IMU
+numSamples = 0  # Numero de muestras tomadas
+datosGx = 0     # Variable donde almacenar el array de muestras.
+datosGy = ""    # Variable donde almacenar el array de muestras.
+datosGz = ""    # Variable donde almacenar el array de muestras.
+stopthread3 = 0  # VFlag de parada de los procesos
 stopthread2 = 0
 stopthread1 = 0
 accelArrx = []
-offsetX = 0     #Variable del valor de offset para Eje X
+offsetX = 0     # Variable del valor de offset para Eje X
 rawGx = ""
+velocidad = 0.0
+velanterior = 0
+distancia = 0.0
+flag = 0
+t = [0,0,0]
+accX = 0
 
 SETTINGS_FILE = "RTIMULib"
 
 print("Using settings file " + SETTINGS_FILE + ".ini")
 if not os.path.exists(SETTINGS_FILE + ".ini"):
-  print("Settings file does not exist, will be created")
+    print("Settings file does not exist, will be created")
 
 s = RTIMU.Settings(SETTINGS_FILE)
 imu = RTIMU.RTIMU(s)
@@ -60,103 +69,157 @@ poll_interval = imu.IMUGetPollInterval()
 print("Recommended Poll Interval: %dmS" % poll_interval)
 
 #########################################################################
-#Declaracion Thread SaveData
+# Declaracion Thread SaveData
+etapes = 20
+historyX = [0] * etapes
+last_index = 0
+coef = ar.array('i', [
+     -526,   -720,   -727,   -449,    164,   1090,   2232,   3424,   4471,
+     5189,   5444,   5189,   4471,   3424,   2232,   1090,    164,   -449,
+     -727,   -720,   -526])
+
+
+def SampleFilter_put():
+    global historyX, last_index
+    if (last_index == etapes):
+        last_index = 0
+    historyX[last_index] = Gx
+    last_index = last_index + 1
+
+# Funcion para la convolución
+
+
+def SampleFilter_get():
+    global last_index, coef, accX
+
+    index = last_index
+    i = 0
+    accX = 0
+    for i in range(etapes):
+        if index != 0:
+            index = index - 1
+        else:
+            index = etapes -1
+        accX = accX + (historyX[index] * coef[i])
+    accX = (accX / pow(2, 16))*2
+    #print ("accX = %f , accY = %f , accZ = %f" %(accX, accY, accZ), end = '\r')
+    return
+
 class SaveDataTrhead (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-#Rutina de autoejecución
+# Rutina de autoejecución
     def run(self):
 
-        global stopthread2,stopthread1, stopthread3,offsetX
+        global stopthread2, stopthread1, stopthread3, offsetX, velocidad, distancia
 
         print ("\nStarting " + self.name)
-        #Se crea un fichero de datos nuevo vacio.
-        if os.path.exists("GxData.txt"):
-            print("\nSe elimina GxData.txt anterior.")
-            os.remove("GxData.txt")
+        # Se crea un fichero de datos nuevo vacio.
+        if os.path.exists("GxRawData.txt"):
+            print("\nSe elimina GxRawData.txt anterior.")
             os.remove("GxRawData.txt")
         print("\nSe crea : GxData.txt")
-        fGx = open("GxData.txt", "a")
         fRawGx = open("GxRawData.txt", "a")
         print("\nEsperando joystick para opcion:")
-        #Bucle de captura de entradas del joystick.
+        # Bucle de captura de entradas del joystick.
         while stopthread3 == 0:
             event = sense.stick.wait_for_event()
-            #Boton central: Calibrar acelerometro en posicion de reposo.
+            # Boton central: Calibrar acelerometro en posicion de reposo.
             if (event.direction == "middle"):
                 suma = 0
                 for x in accelArrx:
                     suma = suma + x
                 offsetX = suma / len(accelArrx)
-                print("\nOffset en X = %f."% offsetX, end='\n')
-            #Otro Boton: Guardar fichero y cerrar programa.
+                velocidad = 0
+                distancia = 0
+                print("\nOffset en X = %f." % offsetX)
+            # Otro Boton: Guardar fichero y cerrar programa.
             else:
-                fGx.write(datosGx)
                 fRawGx.write(rawGx)
                 print ("\nExiting " + self.name)
                 stopthread2 = 1
                 time.sleep(500/1000)
                 stopthread1 = 1
-                stopthread3 =  1
+                stopthread3 = 1
 
-#Declaracion Thread 1
+# Declaracion Thread 1
+
+
 class myThread (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-   def run(self):
-      #Se ejecuta la función de adquisición de muestras
-      print ("Starting " + self.name)
-      while stopthread1 == 0:
-          adquisitionData(15)
-      print ("\nExiting " + self.name)
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        # Se ejecuta la función de adquisición de muestras
+        print ("Starting " + self.name)
+        while stopthread1 == 0:
+            adquisitionData(15)
+        print ("\nExiting " + self.name)
+
+# Declaracion Thread 2
 
 
-#Declaracion Thread 2
 class myThread2 (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-   def run(self):
-      print ("Starting " + self.name)
-      while stopthread2 == 0:
-          Dataget(12)
-      print ("\nExiting " + self.name)
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
 
-#Funcion del thread 1
+    def run(self):
+        print ("Starting " + self.name)
+        while stopthread2 == 0:
+            Dataget(12)
+        print ("\nExiting " + self.name)
+
+# Funcion del thread 1
+
+
 def adquisitionData(tiempo):
     global datosGx, datosGy, datosGz, numSamples, accelArrx, Gx, rawGx
+    global velocidad, distancia, flag, t, accX
     offsetGx = 0
     t0 = time.time()
-    accelArrx.append (Gx)
-    #secorrige el valor de Gx
+    if offsetX == 0:
+        accelArrx.append(accX)
+    # secorrige el valor de Gx
     if (offsetX != 0):
-        offsetGx = Gx - offsetX
-        if (abs(offsetGx) < 0.005):
+        offsetGx = accX - offsetX
+
+        # poner filtro pasa bajos a offsetGX
+
+        if (abs(offsetGx) < 0.02):
             offsetGx = 0
-        datosGx += ("%f, " % (offsetGx))
-        rawGx  += ("%f, " % (Gx))
-        numSamples = numSamples +1
-    #Se setean el flag de thread para permitir al trhead de adquisición, tomar el dato.
+        rawGx += ("%f, " % (offsetGx))
+        #rawGx += ("%f, " % (Gx))
+        numSamples = numSamples + 1
+# Se setean el flag de thread para permitir al trhead de adquisición, tomar el dato.
     event.set()
     time.sleep(tiempo/1000.0)
-    lapsetime = ((time.time() - t0) * 1000 )
-    print("Gx: %1.4f Gy: %1.4f Gz: %1.4f - #Samples = %i SampleTime = %f ms - IMUTime = %f" % (Gx-offsetX,Gy,Gz,numSamples,lapsetime, timeIMU), end = '\r')
-#Funcion del thread 2
-def Dataget (tiempo):
+    lapsetime = ((time.time() - t0) * 1000)
+    deltav = ((offsetGx * 9.8 * (lapsetime / 1000)))
+    velocidad = velocidad + deltav
+    if (offsetGx == 0) and (abs(velocidad) < 0.05):
+        velocidad = 0
+    distancia = distancia + (velocidad * lapsetime/1000)
+    #print("Velocidad = %f m/s, Distancia = %f m - #Samples = %i SampleTime = %2.2f ms" % (velocidad, distancia, numSamples, lapsetime), end = '\r')
+    print("Velocidad = %f m/s, Distancia = %f m - #Samples = %i SampleTime = %2.2f ms, Gx: %f "  % ( velocidad, distancia, numSamples, lapsetime, accX - offsetX), end = '\r')
+# Funcion del thread 2
+
+
+def Dataget(tiempo):
     global Gx, Gy, Gz, timeIMU
-    event.wait() # Blocks until the flag becomes true.
+    event.wait()  # Blocks until the flag becomes true.
     t0 = time.time()
     i = 0
     while (imu.IMURead() == 0):
         time.sleep(0.25/1000.0)
 
     data = imu.getIMUData()
-    #Se obtiene el dato de aceleracion en la variable accel
+    # Se obtiene el dato de aceleracion en la variable accel
     accel = data["accel"]
     Gx = accel[0]
     Gy = accel[1]
@@ -164,7 +227,10 @@ def Dataget (tiempo):
     timeIMU = (time.time() - t0) * 1000
     if (timeIMU > (tiempo)):
         print ("IMU Delay.")
-    event.clear() # Resets the flag.
+    event.clear()  # Resets the flag.
+    SampleFilter_put()
+    SampleFilter_get()
+
 
 # Create new threads
 thread1 = myThread(1, "Thread-1")
